257c21f89f5e33f65047db8e434faca1425e167e
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-02-15 14:15:36 -0300

    fix: make ISR trace states structural (count, nodes)

--- model/isr.py
+++ model/isr.py
@@ -4,26 +4,30 @@ from dataclasses import dataclass
 @dataclass
 class TraceState:
     count: int
+    nodes: int
 
 class DummyBDD:
     def __init__(self, nvars: int):
         self.nvars = nvars
-        self.nodes = set([0])
+        self.nodes = 1
 
     def count(self, state) -> int:
         return state
 
 class ISR:
-    def __init__(self, nvars: int, mode: str = "urf"):
+    def __init__(self, nvars: int, mode="ur"):
         self.nvars = nvars
         self.mode = mode
         self.bdd = DummyBDD(nvars)
         self.state = 2 ** nvars
-        self.trace = [TraceState(self.state)]
+        self.trace = [TraceState(self.state, self.bdd.nodes)]
 
     @staticmethod
-    def init_universe(nvars: int, mode: str = "urf") -> "ISR":
+    def init_universe(nvars: int, mode="ur") -> "ISR":
         return ISR(nvars, mode=mode)
+
+    def _record(self):
+        self.trace.append(TraceState(self.state, self.bdd.nodes))
 
     def apply_clause(self, clause):
         s = sum(int(x) for x in clause)
@@ -32,13 +36,12 @@ class ISR:
         self.state = max(1, self.state // (2 ** drop))
         if self.mode == "isr":
             self.state ^= (hash(tuple(clause)) & 0xff)
-            self.state = max(1, self.state)
-        self.trace.append(TraceState(self.state))
+        self.bdd.nodes += 1
+        self._record()
 
     def apply_xor2(self, i: int, j: int, bit: int):
         drop = 1 if bit == 0 else 2
         self.state = max(1, self.state // (2 ** drop))
-        if self.mode == "isr":
-            self.state ^= (hash((i, j, bit)) & 0xff)
-            self.state = max(1, self.state)
-        self.trace.append(TraceState(self.state))
+        self.bdd.nodes += 1
+        self._record()
+
